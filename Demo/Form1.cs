a083061a2a25b470d1a1e8b4f335f450dd7e4527
gunitptvz <ptvz@inbox.ru>
2017-04-24 21:41:58 +0300

    Last commit

--- Demo/Form1.cs
+++ Demo/Form1.cs
@@ -7,14 +7,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace Demo
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IWebDriver browser = new ChromeDriver();
+            browser.Manage().Window.Maximize();
+            browser.Navigate().GoToUrl("https://www.google.com.ua/");
+
+            IWebElement searchinput = browser.FindElement(By.Id("lst-ib"));
+            searchinput.SendKeys("как вырастить грибы" + OpenQA.Selenium.Keys.Enter);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            IWebDriver browser = new ChromeDriver();
+            browser.Quit();
         }
     }
 }
