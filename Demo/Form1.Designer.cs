215df48e9b46985f8f3181b3803308e76c53cd01
gunitptvz <ptvz@inbox.ru>
2017-04-26 20:57:43 +0300

    last commit 26/04/2017

--- Demo/Form1.Designer.cs
+++ Demo/Form1.Designer.cs
@@ -30,21 +30,25 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(68, 12);
+            this.button1.Location = new System.Drawing.Point(394, 186);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(126, 65);
+            this.button1.Size = new System.Drawing.Size(140, 65);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Open web browser";
+            this.button1.Text = "Open web browser and use Google search";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(68, 132);
+            this.button2.Location = new System.Drawing.Point(555, 307);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(126, 73);
             this.button2.TabIndex = 1;
@@ -52,16 +56,60 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(555, 71);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 65);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Open web browser";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(699, 187);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(138, 64);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Find element";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(79, 345);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(129, 78);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Перебор элементов";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(332, 323);
+            this.textBox1.TabIndex = 5;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(849, 455);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +117,10 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Demo/Form1.cs
+++ Demo/Form1.cs
@@ -24,8 +24,6 @@ namespace Demo
 
         private void button1_Click(object sender, EventArgs e)
         {
-            browser = new OperaDriver();
-            browser.Manage().Window.Maximize();
             browser.Navigate().GoToUrl("https://www.google.com.ua");
 
             IWebElement searchinput = browser.FindElement(By.XPath(".//input[@id='lst-ib']"));
@@ -35,6 +33,52 @@ namespace Demo
         private void button2_Click(object sender, EventArgs e)
         {
            browser.Quit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            browser = new FirefoxDriver();
+            browser.Manage().Window.Maximize();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // IWebElement element;
+
+            // Поиск по ID.
+            // browser.Navigate().GoToUrl("http://yandex.com.ua");
+            // element = browser.FindElement(By.Id("text"));
+            // element.SendKeys("тест");
+
+            // Поиск по ClassName.
+            // browser.Navigate().GoToUrl("http://mail.ru");
+            // element = browser.FindElement(By.ClassName("tb__close__icon-25614543"));
+            // element.Click();
+
+            // Поиск по тексту ссылки.
+            // browser.Navigate().GoToUrl("http://yandex.com.ua");
+            // element = browser.FindElement(By.LinkText("Блог"));
+            // element.Click();
+
+            // Поиск по частичному тексту ссылки.
+            // browser.Navigate().GoToUrl("http://yandex.com.ua");
+            // element = browser.FindElement(By.PartialLinkText("Марк"));
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // browser.Navigate().GoToUrl("http://yandex.com.ua");
+
+            // List<IWebElement> news = browser.FindElements(By.XPath(".//*[@id='tabnews_newsc']//li")).ToList();
+
+            // int a = 1;
+
+            // for(int i = 0; i < news.Count; i++)
+            // {
+            // textBox1.AppendText((a++) + "." + news[i].Text + "." + "\n");
+            // }
+
         }
     }
 }
